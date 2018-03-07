949b49142cbb15012442b5650c83d7dce4e68ea4
仲崇滔 <2593364535@qq.com>
2018-03-07 10:24:14 +0800

    删除无用的常量

--- soho/domain/system/SystemConstant.cs
+++ soho/domain/system/SystemConstant.cs
@@ -7,12 +7,6 @@ namespace soho.domain.system
     {
         public static List<FrontStyleInfo> FrontStyleList { get; } = new List<FrontStyleInfo>();
 
-        /// <summary>
-        ///     纸张尺寸列表
-        /// </summary>
-        public static List<string> PaperSizeList { get; } = new List<string>();
-
-
         public static void InitConstant()
         {
             //异步获取正面样式
@@ -26,7 +20,6 @@ namespace soho.domain.system
                         Name = frontStyle.Name
                     });
                 });
-                //PostCardFrontStyleGridLookUpEdit.DataSource = envelopeFrontStyle.Properties.DataSource = _frontStyles;
             });
         }
     }
