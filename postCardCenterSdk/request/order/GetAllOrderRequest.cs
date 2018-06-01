a958c6dcd598255dfcde92b5bf8689aa11038a60
仲崇滔 <2593364535@qq.com>
2018-06-01 09:12:24 +0800

    提交

---
+++ postCardCenterSdk/request/order/GetAllOrderRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace postCardCenterSdk.request.order
+{
+    class GetAllOrderRequest
+    {
+
+        [JsonProperty("startDate")]
+        public DateTime StarDateTime { get; set; }
+
+        [JsonProperty("endDate")]
+        public DateTime EndDateTime { get; set; }
+    }
+
+}
---
+++ postCardCenterSdk/request/postCard/PostCardProductFileIdSubmitRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace postCardCenterSdk.request.postCard
+{
+    public class PostCardProductFileIdSubmitRequest
+    {
+        public string PostCardId { get; set; }
+        public string ProductFileId { get; set; }
+    }
+}
