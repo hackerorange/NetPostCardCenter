a3b683cd35ac4c456c045c57aef5bb17934bef88
zhongct <zhongct@glodon.com>
2020-01-01 15:40:58 +0800

    处理器添加配置文件

--- PhotoShopProcessor/PostCardQueueProcessor/Program.cs
+++ PhotoShopProcessor/PostCardQueueProcessor/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 using DevExpress.UserSkins;
 using DevExpress.Skins;
 using DevExpress.LookAndFeel;
 using System.Threading;
+using DevExpress.XtraEditors;
 using Hacker.Inko.Net.Base;
 using Inko.Security;
 
@@ -30,6 +34,43 @@ namespace PostCardQueueProcessor
 
                     BonusSkins.Register();
                     SkinManager.EnableFormSkins();
+
+                    if (Process.GetCurrentProcess().MainModule is ProcessModule processModule)
+                    {
+                        var dictionary = new Dictionary<string, string>();
+                        var fileInfo = new FileInfo(processModule.FileName);
+                        fileInfo = new FileInfo(fileInfo.DirectoryName + "/inkoConfig.ini");
+                        if (fileInfo.Exists)
+                        {
+                            var streamReader = new StreamReader(new FileStream(fileInfo.FullName, FileMode.Open));
+                            while (!streamReader.EndOfStream)
+                            {
+                                var line = streamReader.ReadLine();
+                                if (line == null) continue;
+                                var currentSplit = line.Split('=');
+                                if (currentSplit.Length == 2)
+                                {
+                                    dictionary.Add(currentSplit[0], currentSplit[1]);
+                                }
+                            }
+                            streamReader.Close();
+
+                            var host = dictionary["host"];
+                            if (string.IsNullOrEmpty(host))
+                            {
+                                XtraMessageBox.Show("没有配置host，无法初始化网络请求");
+                            }
+                            else
+                            {
+                                if (host.EndsWith("/"))
+                                {
+                                    host = host.Substring(0, host.Length - 1);
+                                }
+
+                                NetGlobalInfo.Host = host;
+                            }
+                        }
+                    }
 
                     var token = InkoSecurityContext.GetToken();
                     if (!string.IsNullOrEmpty(token))
